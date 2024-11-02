e28df1d863a0ff0391caf00a3ac07f63e6518d6a
eric.gulatee <eric.gulatee@gmail.com>
2024-11-02 11:51:32 -0400

    Handle nextjs phase

--- src/environmentvariables.ts
+++ src/environmentvariables.ts
@@ -37,9 +37,13 @@ export function getEnvironmentVariableThrowingException(key: string): string  {
 export function getEnvironmentVariableThrowingExceptionForNextJs(key: string): string  {
   const nextphase = process.env["NEXT_PHASE"]
 
-  if (process.env[key] == undefined) {
-    log.warn("No environment variable found for key=[" + key + "] in NextPhase=[" + nextphase +"]" );
-    throw new Error("No environment variable found for key=[" + key + "] in NextPhase=[" + nextphase +"]" );
+  if (nextphase == "phase-production-build") {
+    const value = process.env[key];
+    log.debug(      "Environment Variable key[" + key + "] has value=[" + value + "] in Next_Phase=[" + nextphase + "]");
+    return value == undefined ?  "" : value;    
+  } else if (process.env[key] == undefined) {
+    log.warn("No environment variable found for key=[" + key + "]")
+    throw new Error("No environment variable found for key=[" + key + "]");
   } else if (process.env[key] == "") {
     log.warn("Empty environment variable found for key=[" + key + "]");
     throw new Error("Empty environment variable found for key=[" + key + "]");
